621f675f46a54d1a3742b3b6e0f1c339aa8a88de
Syrus <syrusamadeu@hotmail.com>
2020-03-23 14:41:36 -0300

    environment config test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  url: 'http://localhost:8080',
+  url: 'https://music-app-spring.azurewebsites.net',
   // url: 'https://music-app-spring.azurewebsites.net',
   production: false
 };
